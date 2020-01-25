1e11f447948bbb6a4a5752bfd4b48136e92a6523
feynmanpan <feynmanpan@gmail.com>
2020-01-25 14:44:28 +0800

    sqlite 2 mysql

--- wtb/mainsite/models.py
+++ wtb/mainsite/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.CharField(max_length=200)
     body = models.TextField()
-    pub_date = models.DateTimeField(default=timezone.now)
+    pub_date = models.DateTimeField(default=timezone.now,verbose_name='張貼日期')
 
     class Meta:
         ordering = ('-pub_date',)
--- wtb/wtb/settings.py
+++ wtb/wtb/settings.py
@@ -78,10 +78,25 @@ WSGI_APPLICATION = 'wtb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pydb',
+        'USER': 'root',
+        'PASSWORD': '!QAZ2wsx',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
