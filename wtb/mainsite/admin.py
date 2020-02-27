9a9eccaa9a1d7b6c79df6f6630abbbe9a20a6ad9
root <root@conda-20200223.asia-east1-a.c.bigdata-20190713.internal>
2020-02-27 17:36:20 +0800

    bookinfo_schema

--- wtb/mainsite/admin.py
+++ wtb/mainsite/admin.py
@@ -3,7 +3,7 @@ from .models import Bookinfo,Store,Post
 # Register your models here
 
 class BookinfoAdmin(admin.ModelAdmin):
-    list_display=('err','bookid','isbn','title','author','publisher','url_cover')
+    list_display=('bookid','err','isbn','title','author','publisher','url_cover')
 
 class StoreAdmin(admin.ModelAdmin):
     list_display=('name','code','url','url_logo','url_href')
