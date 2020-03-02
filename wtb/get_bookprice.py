d8db295e8307db6008de9a058faaeec2048ed212
root <root@conda-20200223.asia-east1-a.c.bigdata-20190713.internal>
2020-03-02 18:40:43 +0800

    price

--- wtb/get_bookprice.py
+++ wtb/get_bookprice.py
@@ -38,17 +38,24 @@ def get_bookprice(bookid:str='',isbn:str='',store:str='',tryDB=True)->dict:
     
     bookprice={'err':'','bookid':bookid,'isbn':isbn,'store':store}
     tw = pytz.timezone('Asia/Taipei')
+    url_qs={
+        'elite':"http://www.eslite.com/Search_BW.aspx?searchType=&query="
+    }
     
     #1.確認====================================
     #(0)一定要指定店家
     if not store:
         bookprice['err']='nostore'
         return bookprice
+    if store not in url_qs:
+        bookprice['err']='nosuchstore'
+        return bookprice
+        
     #(1)bookid跟isbn至少要有一個
     if not bookid and not isbn:
         bookprice['err']='nobookidisbn'
         return bookprice
-    #(2)根據bookid有無，更新isbn
+    #(2)更新bookid及isbn
     if bookid:
         if type(bookid) is not str or len(bookid)!=10:
             bookprice['err']='wrongbookid'
@@ -56,7 +63,9 @@ def get_bookprice(bookid:str='',isbn:str='',store:str='',tryDB=True)->dict:
         #以重查的isbn為準
         row=Bookinfo.objects.filter(bookid=bookid)
         if row.count()==1:
+            bookprice['bookid']=row.first() #要使用instance，update_or_create才能存           
             bookprice['isbn']=row.first().isbn
+            isbn=row.first().isbn            
         else:
             bookprice['err']='nosuchbookid'
             return bookprice 
@@ -64,37 +73,68 @@ def get_bookprice(bookid:str='',isbn:str='',store:str='',tryDB=True)->dict:
         #沒bookid，就一定要用isbn重查
         row=Bookinfo.objects.filter(isbn=isbn)
         if row.count()==1:        
-            bookprice['bookid']=row.first().bookid
+            bookprice['bookid']=row.first() #要使用instance，update_or_create才能存         
         else:
             bookprice['err']='nosuchisbn'
             return bookprice        
 
-    #2.開始抓====================================
-    #搜尋類型用isbn會找不到，用全站
-    url_q="http://www.eslite.com/Search_BW.aspx?searchType=&query="+isbn
+    #2.開始抓==================================== 
+    #根據store抓不同搜尋頁面
+    url_q=url_qs[store]+isbn
+    print(url_q)
+    #
     fake_header = Headers(
         browser="chrome",  # Generate only Chrome UA
         os="win",  # Generate ony Windows platform
         headers=True  # generate misc headers
     )    
-    UA=fake_header.generate()
-    
-    #try    
-    r = requests.get(url_q, 
-                     headers=UA,
-                     #proxies=proxies,
-                     #cookies=cookies,
-                     timeout=3)    
-    r.encoding='utf8'
-    #print(r.text)
-    #
-    doc=pq(r.text)    
-        
-    count=doc.find("#ctl00_ContentPlaceHolder1_lbTotalResultCount").text()
-    
-    if count=='1':
-        price_sale=doc.find(".summary").eq(0).find(".price_sale font").text()
-        bookprice['price_sale']=price_sale
-    #
-    return bookprice
-    
+    UA=fake_header.generate()    
+    # 
+    try:
+        r = requests.get(url_q, 
+                         headers=UA,
+                         #proxies=proxies,
+                         #cookies=cookies,
+                         timeout=5)    
+        r.encoding='utf8'
+        #
+        doc=pq(r.text)    
+        #________________例外收集________________________________                            
+        #(0)狀態碼400~599        
+        if r.status_code != 200:
+            raise Exception(r.status_code)        
+        #
+        #________________price收集________________________________
+        if store=='elite':
+            count=doc.find("#ctl00_ContentPlaceHolder1_lbTotalResultCount").text()
+        if count=='1':
+            price_sale=doc.find(".summary").eq(0).find(".price_sale font").text()
+        else:
+            raise Exception('count='+count) 
+
+    except Exception as e:
+        error=str(e)
+        if 'timeout' in error:
+            bookprice['err']='timeout'
+        else:    
+            bookprice['err']=error[:50]
+    else:
+        cols=['price_sale']
+        #
+        for col in cols:
+            bookprice[col]=locals()[col]
+    finally:
+        #(1)爬成功或失敗，都存DB
+        bookprice['create_dt']=timezone.now() #django timezone會抓OS的UTC時間
+        where={'bookid':bookprice['bookid'],#要用instance來指定
+               'store':store,#bookid+store決定唯一一筆，update_or_create是get
+               'defaults':bookprice}
+        row, create = Bookprice.objects.update_or_create(**where)          
+        #(2)整理回傳
+        #bookinfo['tryDB']=tryDB
+        #bookinfo['fromDB']=False
+        bookprice['create']=create
+        #回傳顯示CST台北時間
+        bookprice['create_dt']=bookprice['create_dt'].astimezone(tw)
+        #
+        return bookprice    
