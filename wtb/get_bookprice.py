da92f964c7b72fd78861c782425468fc4bcd9038
root <root@conda-20200325-beforehttps.asia-east1-a.c.bigdata-20200223.internal>
2020-03-29 18:58:00 +0800

    yahoo price

--- wtb/get_bookprice.py
+++ wtb/get_bookprice.py
@@ -362,7 +362,8 @@ def get_bookprice(bookid:str='',isbn:str='',store:str='',tryDB=True)->dict:
                 url_book="https://www.sanmin.com.tw"+url_book   
         #(10)Yahoo
         elif store=='yahoo':
-            price_sale=doc.find(".gridList span.BaseGridItem__price___31jkj em").eq(0).text().replace('$','') or ''
+            #有時價錢在span>em，有時沒有
+            price_sale=doc.find(".gridList span.BaseGridItem__price___31jkj").eq(0).text().replace('$','').strip() or ''
             url_book=doc.find(".gridList a.BaseGridItem__content___3LORP").eq(0).attr("href") or ''  
         #(11)聯經
         elif store=='linking':
