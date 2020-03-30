4ee39decac8213e0b1e2a1ca637b4812e0dcccea
root <root@conda-20200325-beforehttps.asia-east1-a.c.bigdata-20200223.internal>
2020-03-30 20:38:12 +0800

    admin search info bookid

--- wtb/mainsite/admin.py
+++ wtb/mainsite/admin.py
@@ -4,7 +4,7 @@ from .models import Bookinfo,Bookprice,Store,Post
 
 class BookinfoAdmin(admin.ModelAdmin):
     list_display=('bookid','err','isbn','title','author','publisher','pub_dt','price_list','price_sale','price_sale_ebook','bookid_ebook','spec','create_dt')
-    search_fields=('err',)
+    search_fields=('err','bookid')
 
 class BookpriceAdmin(admin.ModelAdmin):
     list_display=('bookid','err','isbn','store','price_sale','price_sale_ebook','url_book','url_ebook','create_dt')
