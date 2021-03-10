1ca90b161ca6d19a9bc95dbd36bfad3a6a30173e
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-03-10 17:18:33 +0800

    5002

--- fast_api/main_5002.py
+++ fast_api/main_5002.py
@@ -1,15 +1,22 @@
-from fastapi import FastAPI
 from typing import Optional, Callable
-from views_5002 import test
 #
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+#
+from views_5002 import test
+#####################################################
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def path_get(url: str, func: Callable):
+    # return app.get(url, response_class=HTMLResponse)(func)
     return app.get(url)(func)
 
 
 # urlpattern
-path_get("/", test)
+path_get("/test/{p}", test)
