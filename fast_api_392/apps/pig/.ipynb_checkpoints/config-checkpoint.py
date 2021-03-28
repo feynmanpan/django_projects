ca42fb1209d24ebf4cbb7cfcc22cab120d23660b
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-03-28 23:11:14 +0800

    good proxy

--- fast_api_392/apps/pig/.ipynb_checkpoints/config-checkpoint.py
+++ fast_api_392/apps/pig/.ipynb_checkpoints/config-checkpoint.py
@@ -1,23 +1,23 @@
 from collections import namedtuple
 import os
 import aiohttp
-# 
+#
 from fastapi.templating import Jinja2Templates
 ###########################################################
 
 # 規格豬
-cacert = True
+cacert = [False, True][1]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
 }
 url_pig = "https://ppg.naif.org.tw/naif/MarketInformation/Pig/FPSCompare.aspx"
 url_pig2 = "http://ppg.naif.org.tw/naif/MarketInformation/Pig/TranStatistics.aspx"  # 單日多市場價量比較
-# 
+#
 cwd = os.path.dirname(os.path.realpath(__file__))
 dmy = ['d', 'm', 'y']
 pig_csv_path = namedtuple('pig_csv', dmy)(*[os.path.join(cwd, f'pig_{tmp}.csv') for tmp in dmy])
 templates = 'templates'
-jinja_templates = Jinja2Templates(directory=os.path.join(cwd,templates))
+jinja_templates = Jinja2Templates(directory=os.path.join(cwd, templates))
 
 #
 year_format = '%Y'
--- fast_api_392/apps/pig/config.py
+++ fast_api_392/apps/pig/config.py
@@ -6,7 +6,7 @@ from fastapi.templating import Jinja2Templates
 ###########################################################
 
 # 規格豬
-cacert = [True, False][0]
+cacert = [False, True][1]
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
 }
