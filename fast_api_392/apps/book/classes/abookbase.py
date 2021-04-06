300e5846dd93a9e0a6c89094e815ec57b4452f63
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-04-07 00:32:53 +0800

    base price_update

--- fast_api_392/apps/book/classes/abookbase.py
+++ fast_api_392/apps/book/classes/abookbase.py
@@ -14,16 +14,15 @@ class VALIDATE(ABCMeta):
     def __new__(cls, name, bases, class_dict):
         # 不驗證 BOOKBASE
         if (base := bases[0]) != object:
-            # 從 BOOKBASE 抽info_cols
+            # 檢查子類info_default的key
             info_cols = base.info_cols
-            # 檢查info_default的key
             info_default = class_dict.get('info_default', {})
             set0 = set(info_cols)
             set1 = set(info_default.keys())
             if (rest := set1-set0) != base.empty:
                 raise KeyError(f'info_default中，欄位{rest}不在BOOKBASE的info_cols裡面')
             # 造子類的預設info並assign，以子類類名為store名稱
-            info_default = dict(zip(info_cols, [None]*len(info_cols))) | info_default
+            info_default = base.info_default | info_default
             info_default[base.INFO_COLS.store] = name
             class_dict['info_default'] = info_default
         #
@@ -45,7 +44,8 @@ class BOOKBASE(object, metaclass=VALIDATE):
     ]
     INFO_COLS = namedtuple('INFO_COLS', info_cols)(*info_cols)
     #
-    info_default = {}
+    info_default = dict.fromkeys(info_cols)  # dict(zip(info_cols, [None]*len(info_cols)))
+    #
     bookid_pattern = ''
     int_pattern = '^[0-9]+$'
     float_pattern = r'^[0-9]*\.*[0-9]*$'
