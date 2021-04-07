6e68a0c22ee5d05a5f994a3d1d95ffb7f3aa1dfc
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-04-07 17:08:02 +0800

    base price_update

---
+++ fast_api_392/apps/sql/model.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+#
+from .database import Base
+##############################################################
+
+
+class TBBOOKS(Base):
+    __tablename__ = "tbbooks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    # items = relationship("Item", back_populates="owner")
