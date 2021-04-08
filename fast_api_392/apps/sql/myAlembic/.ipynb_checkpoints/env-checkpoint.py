96987f2709c447331e368d9c61d22dcbdba5d8c0
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-04-08 20:19:42 +0800

    alembic

--- fast_api_392/apps/sql/myAlembic/.ipynb_checkpoints/env-checkpoint.py
+++ fast_api_392/apps/sql/myAlembic/.ipynb_checkpoints/env-checkpoint.py
@@ -4,8 +4,6 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-#
-from model import Base, IPS, TEST
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,11 +17,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata  # None
-# target_metadata = [
-#     IPS.metadata,
-#     TEST.metadata,
-# ]
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
