4ff570b58907b1acd0d5b04a50f70c77dd6a9fb5
root <root@conda-20210309.asia-east1-a.c.conda-20210309.internal>
2021-04-09 21:10:13 +0800

    crud

---
+++ fast_api_392/apps/book/.ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,14 @@
+import os
+#
+from fastapi.templating import Jinja2Templates
+#######################################
+
+dt_format = "%Y-%m-%d_%H:%M:%S"
+pub_dt_format = "%Y-%m-%d"
+#
+cwd = os.path.dirname(os.path.realpath(__file__))
+templates = 'templates'
+jinja_templates = Jinja2Templates(directory=os.path.join(cwd, templates))
+#
+timeout = 7
+update_errcnt_max = 5
